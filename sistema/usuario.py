12dbaa69bf148f08ed25e4a0c0dbcc2798bf6e20
Mylena Rocha <138344284+b3ery@users.noreply.github.com>
2025-11-24 23:12:39 -0300

    Mylena Rocha: Adicionando Comentários a classe usuario

--- sistema/usuario.py
+++ sistema/usuario.py
@@ -1,11 +1,17 @@
 class Usuario:
-    def __init__(self, id_usuario=None, nome=None, email=None, telefone=None, ativo=True, id=None, **kwargs):
+    def init(self, id_usuario=None, nome=None, email=None, telefone=None, ativo=True, id=None, **kwargs):
+        # Garante compatibilidade entre JSON e sistema
         self.id = id if id is not None else id_usuario
+
+        # Dados do usuário
         self.nome = nome
         self.email = email
         self.telefone = telefone
+
+        # Indica se o usuário está ativo no sistema
         self.ativo = ativo
 
-    def __str__(self):
+    def str(self):
+        # Exibe dados do usuário resumidamente
         status = "Ativo" if self.ativo else "Inativo"
         return f"[{self.id}] {self.nome} | {self.email} | {status}"
