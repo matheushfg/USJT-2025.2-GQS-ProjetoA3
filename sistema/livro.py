dbdc389724d0e9c5b960de7ee1ad6667395ce919
Marcos Santos <marcosaraujosantoz@gmail.com>
2025-11-24 23:33:57 -0300

    Marcos: Adicionando comentários na Classe livro.py

--- sistema/livro.py
+++ sistema/livro.py
@@ -1,16 +1,18 @@
 class Livro:
     def __init__(self, id_livro=None, titulo=None, autor=None, isbn=None, ano=None, disponivel=True, id=None, **kwargs):
-        # compatibilidade com JSON ("id") e com código interno ("id_livro")
+        # Garante compatibilidade entre ID salvo no JSON e ID interno
         self.id = id if id is not None else id_livro
 
+        # Armazena informações do livro
         self.titulo = titulo
         self.autor = autor
         self.isbn = isbn
         self.ano = ano
 
-        # compatível com JSON salvo anteriormente
+        # Indica se o livro pode ser emprestado
         self.disponivel = disponivel
 
     def __str__(self):
+        # Exibe status do livro
         status = "Disponível" if self.disponivel else "Emprestado"
         return f"[{self.id}] {self.titulo} - {self.autor} ({self.ano}) | {status}"
